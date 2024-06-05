c10e1cda3ea464cf0d8fd6a56bb11eaf26f0129e
arsany199 <arsanykamal30@gmail.com>
2024-06-05 12:39:35 -0700

    0-lockboxes.py

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""function that determines if a box containing a list can be opened using keys"""
+
+
+def canUnlockAll(boxes):
+    """Determines if boxes can be unlocked or not"""
+    place = 0
+    unlocked = {}
+
+    for b in boxes:
+        if len(b) == 0 or place == 0:
+            unlocked[place] = "always_unlocked"
+        for key in b:
+            if key < len(boxes) and key != place:
+                unlocked[key] = key
+        if len(unlocked) == len(boxes):
+            return True
+        place = place + 1
+    return False
