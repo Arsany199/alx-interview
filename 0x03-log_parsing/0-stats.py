ed48d310cbd11ca307365e7127345c6b0db4da2e
arsany199 <arsanykamal30@gmail.com>
2024-06-20 02:48:59 -0700

    0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -18,9 +18,9 @@ if __name__ == "__main__":
                 print("{}: {}".format(k, v))
 
     try:
-        for line in sys.stdin:
+        for l in sys.stdin:
             count = count + 1
-            data = line.split()
+            data = l.split()
             try:
                 status_code = data[-2]
                 if status_code in stat:
